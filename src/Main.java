3f8f45af39b90509fdbf9b9eaebdf1c886c06801
mateojubells <121090104+mateojubells@users.noreply.github.com>
2023-02-13 12:16:54 +0100

    Cartellera amb horaris mig creada

--- src/Main.java
+++ src/Main.java
@@ -1,33 +1,60 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-cartelera();
-
-
-
+    public static void main(String[] args)
+    {
+        cartellera();
     }
 
-    public  static void cartelera(){
-       String cartelera[][] = new String[4][3];
+    private static void cartellera()
+    {
+       String[] cartellera = new String[2];
 
-        cartelera[0][0] = "Llaman a la puerta ";
-        cartelera[0][1] = "El Gato con Botas: El último deseo";
-        cartelera[0][2] = "Astérix y Obélix: El Reino Medio ";
-        cartelera[0][3] = "La niña de la comunión";
-        cartelera[1][0] = "Avatar: El sentido del agua";
-        cartelera[1][1] = "Los Fabelman";
-        cartelera[1][2] = "Titanic (25 aniversario) ";
-        cartelera[1][3] = "El piloto";
-        cartelera[2][0] = "La ballena (The Whale)";
-        cartelera[2][1] = "Babylon";
-        cartelera[2][2] = "El asombroso Mauricio";
-        cartelera[2][3] = "M3GAN";
+        cartellera[0] = "El Gato con Botas: El último deseo";
+        cartellera[1] = "Astérix y Obélix: El Reino Medio";
+        /* cartellera[2] = "Llaman a la puerta ";
+        cartellera[3] = "Todo a la vez en todas partes";
+        cartellera[4] = "Avatar: El sentido del agua";
+        cartellera[5] = "Los Fabelman";
+        cartellera[6] = "Titanic (25 aniversario) ";
+        cartellera[7] = "El piloto";
+        cartellera[8] = "La ballena (The Whale)";
+        cartellera[9] = "Babylon";
+        cartellera[10] = "El asombroso Mauricio";
+        cartellera[11] = "Operación Fortune: El gran engaño";
+        cartellera[12] = "M3GAN"; */
 
+        int[][] horari = new int[12][3];
 
+        horari[0][0] = 0;
+        horari[0][1] = 14;
+        horari[0][2] = 30;
+        horari[1][0] = 0;
+        horari[1][1] = 15;
+        horari[1][2] = 30;
 
+        horari[2][0] = 1;
+        horari[2][1] = 20;
+        horari[2][2] = 30;
 
+        for (int a = 0; a < cartellera.length; a++)
+        {
+            System.out.println(cartellera[a]);
+
+            for (int i = 0; i < horari.length; i++)
+            {
+                if (horari[i][0] == a)
+                {
+                    for (int j = 0; j < horari[j].length; j++)
+                    {
+                        if (horari[i][2] != 0)
+                        {
+                            System.out.println(horari[i][1] + ":" + horari[i][2]);
+                            break;
+                        }
+                    }
+                }
+            }
+        }
     }
-
-
 }
