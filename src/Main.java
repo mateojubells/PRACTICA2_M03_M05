093c6b481612f252987647258bb1ae4d4de567b7
mateojubells <121090104+mateojubells@users.noreply.github.com>
2023-02-13 11:48:56 +0100

    Peliculas

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,33 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+cartelera();
+
+
+
     }
+
+    public  static void cartelera(){
+       String cartelera[][] = new String[4][3];
+
+        cartelera[0][0] = "Llaman a la puerta ";
+        cartelera[0][1] = "El Gato con Botas: El último deseo";
+        cartelera[0][2] = "Astérix y Obélix: El Reino Medio ";
+        cartelera[0][3] = "La niña de la comunión";
+        cartelera[1][0] = "Avatar: El sentido del agua";
+        cartelera[1][1] = "Los Fabelman";
+        cartelera[1][2] = "Titanic (25 aniversario) ";
+        cartelera[1][3] = "El piloto";
+        cartelera[2][0] = "La ballena (The Whale)";
+        cartelera[2][1] = "Babylon";
+        cartelera[2][2] = "El asombroso Mauricio";
+        cartelera[2][3] = "M3GAN";
+
+
+
+
+    }
+
+
 }
