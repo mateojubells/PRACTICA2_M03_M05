ac6eed1dfba353dc885da27f3f7efc8655224a8f
francesc-barcelo <123629537+francesc-barcelo@users.noreply.github.com>
2023-02-15 13:02:36 +0100

    Feat: Menu principal + menu submenu

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import javax.management.MalformedObjectNameException;
 import java.util.Scanner;
 public class Main
 {
@@ -10,31 +11,120 @@ public class Main
         // mateo
         String[] cartellera = new String[12];
         int[][] horari = new int[12][3];
-        cartelleraHorari(cartellera, horari);
-        mostrarCartelleraHoraris(cartellera, horari);
 
-        // francesc
-        final String MENU_PRINCIPAL = " 1. Veure cartellera \n 2. Comprar entrades \n 3. Sortir";
+        final String TITOL = "||||||| CINES IMPERIAL |||||||"; // 30 Char
+        final String MENU_PRINCIPAL = " 1. Veure cartellera \n" + " 2. Comprar entrades \n" + " 3. Sortir";
 
-        System.out.println(MENU_PRINCIPAL);
-        int opcioMenuPrincipal = llegirInt("Escull una opció del menú: ", "ERROR: Opció de menú no vàlida", 1, 3);
+        // Mètode per delcarar les dades del la cartellera i els horaris //
+        cartelleraHorari(horari, cartellera);
+        // mostrarCartelleraHoraris(cartellera, horari);
 
-        if (opcioMenuPrincipal == 3)
+        System.out.println("\n" + TITOL + "\n" + MENU_PRINCIPAL);
+        int opcioMenu = llegirInt("Escull una opció del menú: ", "ERROR: Opció de menú no vàlida", 1, 3);
+
+        if (opcioMenu == 3)
         {
             System.out.println("\n" + "Apagant màquina...");
         }
         else
         {
-            switch (opcioMenuPrincipal)
+            switch (opcioMenu)
             {
                 case 1:
-                    System.out.println("Hola");
+                    menuVeureCartellera(cartellera, horari);
                     break;
                 case 2:
                     break;
             }
 
             menuPrincipal();
+        }
+    }
+
+    private static void menuVeureCartellera(String[] cartellera, int[][] horari)
+    {
+        final String TITOL = "|||||||| VEURE  PELIS ||||||||";
+        final String MENU_CARTELLERA = " 1. Veure cartellera \n" + " 2. Veure noms pel·lícules \n" + " 3. Buscar pel·lícula \n" + " 4. Buscar segons hora \n" + " 5. Tornar al inici";
+
+        System.out.println("\n" + TITOL + "\n" + MENU_CARTELLERA);
+        int opcioMenu = llegirInt("Escull una opció del menú: ", "ERROR: Opció de menú no vàlida", 1, 4);
+
+        if (opcioMenu == 5)
+        {
+            System.out.println("\n" + "Tornant al menú principal...");
+        }
+        else
+        {
+            switch (opcioMenu)
+            {
+                case 1:
+                    System.out.print("\n" + "||||||||| CARTELLERA |||||||||");
+                    mostrarCartelleraHoraris(horari, cartellera);
+                    break;
+            }
+
+            menuVeureCartellera(cartellera, horari);
+        }
+    }
+    private static void cartelleraHorari(int[][] horari, String[] cartellera)
+    {
+        cartellera[0] = "El Gato con Botas: El último deseo";
+        cartellera[1] = "Astérix y Obélix: El Reino Medio";
+        cartellera[2] = "Llaman a la puerta ";
+        cartellera[3] = "Todo a la vez en todas partes";
+        cartellera[4] = "Avatar: El sentido del agua";
+        cartellera[5] = "Los Fabelman";
+        cartellera[6] = "Titanic (25 aniversario) ";
+        cartellera[7] = "El piloto";
+        cartellera[8] = "La ballena (The Whale)";
+        cartellera[9] = "Babylon";
+        cartellera[10] = "El asombroso Mauricio";
+        cartellera[11] = "Operación Fortune: El gran engaño";
+
+
+        horari[0][0] = 0;
+        horari[0][1] = 14;
+        horari[0][2] = 30;
+
+        horari[1][0] = 1;
+        horari[1][1] = 15;
+        horari[1][2] = 30;
+        horari[2][0] = 1;
+        horari[2][1] = 20;
+        horari[2][2] = 30;
+        horari[3][0] = 1;
+        horari[3][1] = 22;
+        horari[3][2] = 15;
+        horari[4][0] = 2;
+        horari[4][1] = 15;
+        horari[4][2] = 15;
+        horari[5][0] = 3;
+        horari[5][1] = 15;
+        horari[5][2] = 15;
+        horari[6][0] = 4;
+    }
+    private static void mostrarCartelleraHoraris(int[][] horari, String[] cartellera)
+    {
+        for (int a = 0; a < cartellera.length; a++)
+        {
+            System.out.print("\n" + cartellera[a] + " - ");
+
+            for (int i = 0; i < horari.length; i++)
+            {
+                if (horari[i][0] == a)
+                {
+                    for (int j = 0; j < horari[j].length; j++)
+                    {
+                        if (horari[i][2] != 0)
+                        {
+                            System.out.print(horari[i][1] + ":" + horari[i][2] + " ");
+                            break;
+                        }
+                    }
+                }
+            }
+
+            System.out.println();
         }
     }
     private static int llegirInt(String missatge, String error, int min, int max)
@@ -100,67 +190,5 @@ public class Main
         } while (!controlErrors);
 
         return valor;
-    }
-    private static void cartelleraHorari(String[] cartellera, int[][] horari)
-    {
-        cartellera[0] = "El Gato con Botas: El último deseo";
-        cartellera[1] = "Astérix y Obélix: El Reino Medio";
-        cartellera[2] = "Llaman a la puerta ";
-        cartellera[3] = "Todo a la vez en todas partes";
-        cartellera[4] = "Avatar: El sentido del agua";
-        cartellera[5] = "Los Fabelman";
-        cartellera[6] = "Titanic (25 aniversario) ";
-        cartellera[7] = "El piloto";
-        cartellera[8] = "La ballena (The Whale)";
-        cartellera[9] = "Babylon";
-        cartellera[10] = "El asombroso Mauricio";
-        cartellera[11] = "Operación Fortune: El gran engaño";
-
-
-        horari[0][0] = 0;
-        horari[0][1] = 14;
-        horari[0][2] = 30;
-
-        horari[1][0] = 1;
-        horari[1][1] = 15;
-        horari[1][2] = 30;
-        horari[2][0] = 1;
-        horari[2][1] = 20;
-        horari[2][2] = 30;
-        horari[3][0] = 1;
-        horari[3][1] = 22;
-        horari[3][2] = 15;
-        horari[4][0] = 2;
-        horari[4][1] = 15;
-        horari[4][2] = 15;
-        horari[5][0] = 3;
-        horari[5][1] = 15;
-        horari[5][2] = 15;
-        horari[6][0] = 4;
-    }
-
-    private static void mostrarCartelleraHoraris(String[] cartellera, int[][] horari)
-    {
-        for (int a = 0; a < cartellera.length; a++)
-        {
-            System.out.println("\n" + cartellera[a]);
-
-            for (int i = 0; i < horari.length; i++)
-            {
-                if (horari[i][0] == a)
-                {
-                    for (int j = 0; j < horari[j].length; j++)
-                    {
-                        if (horari[i][2] != 0)
-                        {
-                            System.out.print(horari[i][1] + ":" + horari[i][2] + " ");
-                            break;
-                        }
-                    }
-                }
-            }
-
-            System.out.println();
-        }
     }
 }
